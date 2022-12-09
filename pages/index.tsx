e041d19a063c879bfc6365e80c877950d47e9c7b
Javier Cepeda <erjavierc@gmail.com>
2022-12-09 08:08:46 -0500

    adding new page (project)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import porfilePic from '../images/id.jpg'
+import Navbar from './api/navbar'
 
 export default function Home() {
   return (
@@ -17,13 +18,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
 
       </Head>
-      <nav>
-        <a href='#!'>Web Developer</a>
-        <ul>
-          <li>Contact</li>
-          <li>Projects</li>
-        </ul>
-      </nav>
+
+      <Navbar />
 
       <main className={styles.main}>
         
---
+++ pages/project.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './api/navbar';
+
+function Project() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Project
